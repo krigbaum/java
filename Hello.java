e4f9ffc8dc822946ccc57b6d703fc5f01c14bec4
Larry Krigbaum <krigbaum@purdue.edu>
2016-07-22 14:22:44 -0400

    Added semi-colon

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello
     public static void main(String [] args)
     {
         System.out.println("Hello from Java land!");
-        System.out.println(" and a good day to you!")
+        System.out.println(" and a good day to you!");
     }
 }
